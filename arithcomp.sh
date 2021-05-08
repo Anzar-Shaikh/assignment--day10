8c87a29d3a7e83ee5b57fe9c70855806a13d28d1
Anzar-Shaikh <anzarshaikh373@gmail.com>
2021-05-08 18:33:10 +0530

    one com done

--- arithcomp.sh
+++ arithcomp.sh
@@ -7,3 +7,8 @@ echo WLCOME TO THE ARITHMATIC COMPUTATION AND SORTING
 read -p "ENTER 1st Number :" a
 read -p "ENTER 2nd Number :" b
 read -p "ENTER 3rd Number :" c
+
+d=$( echo $a $b $c | awk '{print $1+$2*$3 }' )
+
+echo $d
+
