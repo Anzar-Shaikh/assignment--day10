77af025fed9037f35cad23c8d1e342f23a471f10
Anzar-Shaikh <anzarshaikh373@gmail.com>
2021-05-08 19:20:34 +0530

    stored the values

--- arithcomp.sh
+++ arithcomp.sh
@@ -1,6 +1,6 @@
 #! /bin/bash  -x
 
-
+declare -A dict
 echo WLCOME TO THE ARITHMATIC COMPUTATION AND SORTING
 
 
@@ -9,15 +9,20 @@ read -p "ENTER 2nd Number :" b
 read -p "ENTER 3rd Number :" c
 
 d=$( echo $a $b $c | awk '{print $1+$2*$3 }' )
-
+dict['d']=$d
 echo $d
 
 e=$( echo $a $b $c | awk '{print $3+$1/$2 }' )
+dict['e']=$e
 echo  $e
 
 f=$( echo $a $b $c | awk '{print $1*$2+$3 }' )
+dict['f']=$f
 echo $f
 
 g=$( echo $a $b $c | awk '{print $1 % $2 + $3 }' )
-
+dict['g']=$f
 echo $g
+
+
+echo ${dict[@]}
